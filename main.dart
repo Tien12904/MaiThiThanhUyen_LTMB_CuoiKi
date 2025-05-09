18cb174c60846ccfd5afdef6256df18d83f9140a
Uyen <1822041206@dntu.edu.vn>
2025-05-09 16:17:34 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import "FlutterTaskManager/providers/user_providers.dart";
+import "FlutterTaskManager/providers/task_providers.dart";
+import "FlutterTaskManager/screens/login_screen.dart";
+import "FlutterTaskManager/screens/task_listscreen.dart";
+// code chạy đc 
+//----------------------------------------------------------------------------------------------------
+void main()
+{
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+        ChangeNotifierProvider(create: (_) => TaskProvider()),
+     ],
+      child: MaterialApp(
+        title: 'Flutter Task Manager',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const AuthWrapper(),
+      ),
+    );
+  }
+}
+
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final userProvider = Provider.of<UserProvider>(context);
+
+    if (userProvider.isLoggedIn) {
+      return TaskListScreen();
+    } else {
+      return LoginScreen();
+    }
+  }
+}
+// code của thầy (không chạy được)
+//----------------------------------------------------------------------------------------------------
+//void main() {
+ //runApp(const MyApp());
+//}
+
+//class MyApp extends StatelessWidget {
+ // const MyApp({super.key});
+
+  // This widget is the root of your application.
+// @override
+//  Widget build(BuildContext context) {
+ //   return MaterialApp(
+ //     title: 'Flutter Demo',
+ //     theme: ThemeData(
+  //      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  //    ),
+ //     home:  NoteListScreen(),
+ //   );
+ // }
+//}
